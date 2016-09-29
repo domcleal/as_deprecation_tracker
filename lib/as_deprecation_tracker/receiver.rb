f4de498d60df5c5d0ea336b0caa89030b7baaf3f
Dominic Cleal <dominic@cleal.org>
2016-09-29 11:15:39 +0100

    Raise DeprecationException for non-whitelisted deprecation

--- lib/as_deprecation_tracker/receiver.rb
+++ lib/as_deprecation_tracker/receiver.rb
@@ -5,6 +5,11 @@ module ASDeprecationTracker
   # Receives deprecation.rails events via ActiveSupport::Notifications
   class Receiver < ::ActiveSupport::Subscriber
     def deprecation(event)
+      return if ASDeprecationTracker.whitelist.matches?(event.payload)
+
+      e = ActiveSupport::DeprecationException.new(event.payload[:message])
+      e.set_backtrace(event.payload[:callstack].map(&:to_s))
+      raise e
     end
   end
 end
--- test/receiver_test.rb
+++ test/receiver_test.rb
@@ -2,8 +2,36 @@
 require 'test_helper'
 
 class ReceiverTest < ASDeprecationTracker::TestCase
+  def test_deprecation_whitelisted
+    whitelist = ASDeprecationTracker::Whitelist.new
+    ASDeprecationTracker.expects(:whitelist).returns(whitelist)
+    stack = caller
+    whitelist.expects(:matches?).with(message: 'deprecated call', callstack: stack).returns(true)
+    ASDeprecationTracker::Receiver.new.deprecation(event(message: 'deprecated call', callstack: stack))
+  end
+
+  def test_deprecation_unknown
+    whitelist = ASDeprecationTracker::Whitelist.new
+    ASDeprecationTracker.expects(:whitelist).returns(whitelist)
+    stack = caller
+    whitelist.expects(:matches?).with(message: 'deprecated call', callstack: stack).returns(false)
+    e = assert_raises(ActiveSupport::DeprecationException) do
+      ASDeprecationTracker::Receiver.new.deprecation(event(message: 'deprecated call', callstack: stack))
+    end
+    assert_equal 'deprecated call', e.message
+    assert_equal stack, e.backtrace
+  end
+
   def test_subscription
     ASDeprecationTracker::Receiver.any_instance.expects(:deprecation)
     ActiveSupport::Notifications.instrument('deprecation.rails', message: 'test')
+  end
+
+  private
+
+  def event(payload)
+    mock('event').tap do |event|
+      event.stubs(:payload).returns(payload)
+    end
   end
 end
