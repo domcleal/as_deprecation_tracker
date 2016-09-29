9694c5423650a1ccb5cf21e463b17df5275da614
Dominic Cleal <dominic@cleal.org>
2016-09-29 11:43:14 +0100

    Only register and configure when active

--- lib/as_deprecation_tracker/railtie.rb
+++ lib/as_deprecation_tracker/railtie.rb
@@ -4,7 +4,7 @@ require 'as_deprecation_tracker/receiver'
 module ASDeprecationTracker
   # Railtie to register for deprecation notifications
   class Railtie < ::Rails::Railtie
-    initializer 'as_deprecation_tracker.deprecation_notifications', after: :load_environment_config do
+    initializer 'as_deprecation_tracker.deprecation_notifications', after: :load_environment_config, if: -> { ASDeprecationTracker.active? } do
       Receiver.attach_to :rails
       ActiveSupport::Deprecation.behavior = :notify if ASDeprecationTracker.config.register_behavior?
     end
